3cb026c343a366b7679bc2f3a871a897541f5df5
levintoo <tooklevin@gmail.com>
2025-08-28 13:18:40 +0300

    feat(ui): initialize shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      hell0
+      <h1>hell0</h1>
+      <Button>Click me</Button> 
     </div>
   );
 }
